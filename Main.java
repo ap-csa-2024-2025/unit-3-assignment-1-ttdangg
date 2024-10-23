5904db1811acb06cdcdf3000065fa88435d558d2
ttdangg <ttdang1445@gmail.com>
2024-10-23 10:13:03 -0700

    boolean rectangle

--- Main.java
+++ Main.java
@@ -4,6 +4,14 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Rectangle rectangle1 = new Rectangle (4,10);
+    Rectangle rectangle2 = new Rectangle (10,4);
+    boolean isRotated = (rectangle1.getLength() == rectangle2.getWidth()) && (rectangle1.getWidth() == rectangle2.getLength());
+    boolean isCongruent = rectangle1.equals(rectangle2) || isRotated; 
+    boolean isSimilar = isCongruent || (rectangle1.getLength() / rectangle2.getLength()) == (rectangle1.getWidth() / rectangle2.getWidth()); 
+    System.out.println(isRotated);
+    System.out.println(isCongruent);
+    System.out.println(isSimilar);
+
   }
 }
